b32f9886827dabb9b14d2e619e0322a5018747e6
dsolerh <dsolerh.cinter95@gmail.com>
2021-11-13 19:59:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const { CacheManager } = require("ds-cache-manager");
-CacheManager.setSize(5);
+// const { CacheManager } = require("ds-cache-manager");
+// CacheManager.setSize(5);
 
-CacheManager.set("test", "d1", { a: 1, b: 1 });
-CacheManager.set("test", "d2", { a: 2, b: 2 });
-CacheManager.set("test", "d3", { a: 3, b: 3 });
-CacheManager.set("test", "d4", { a: 4, b: 4 });
+// CacheManager.set("test", "d1", { a: 1, b: 1 });
+// CacheManager.set("test", "d2", { a: 2, b: 2 });
+// CacheManager.set("test", "d3", { a: 3, b: 3 });
+// CacheManager.set("test", "d4", { a: 4, b: 4 });
 
-require("./other");
+// require("./other");
 
-console.log("🚀 ~ file: index.js ~ line 5 ~ CacheManager", CacheManager);
+// console.log("🚀 ~ file: index.js ~ line 5 ~ CacheManager", CacheManager);
+
+// function Daniel() {
+//     console.log(cache);
+//     var cache;
+//     cache = 'daniel';
+// }
+
+// Daniel();
+
+let a = [1, 2, 3, 4, 4, 5, 6, 7];
+let b = a;
+b[1] = 21;
+console.log(a, b);
