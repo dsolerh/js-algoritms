0c2a28db08bf59212d64ac1b22468e9b2ae6258a
dsolerh <dsolerh.cinter95@gmail.com>
2022-02-09 17:09:43 -0500

    tsting loops of lodash

--- lodash/loop-object.js
+++ lodash/loop-object.js
@@ -1,6 +1,7 @@
 const _ = require("lodash");
 
-const r = _.forOwn({ a: 1, b: 1, c: 1 }, function (v, k) {
+const r = _.map({ a: 1, b: 1, c: 1 }, function (v, k) {
+console.log("🚀 ~ file: loop-object.js ~ line 4 ~ r ~ v, k", v, k)
   return [v, k];
 });
 console.log("🚀 ~ file: basic.js ~ line 8 ~ r", r);
